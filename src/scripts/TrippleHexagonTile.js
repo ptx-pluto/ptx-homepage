d4ce0b6ae93dd9025bca0f116549c9931a8bc838
ptx-pluto <sheep9275@gmail.com>
2014-08-04 00:38:32 +0800

    Tripple Hexagon added

--- src/scripts/TrippleHexagonTile.js
+++ src/scripts/TrippleHexagonTile.js
@@ -22,7 +22,13 @@
                 this.centerY = 1.5*this.edge;
             }
 
-            this.$inner = this.$.polygon(getTripleHexagonArray(this.isUp, this.centerX, this.centerY, this.edge*innerRatio));
+            if (this.isUp) {
+                this.$inner = this.$.polygon(getTripleHexagonArray(this.isUp, this.centerX, this.centerY, this.edge*innerRatio));
+            }
+            else {
+                this.$inner = this.$.polygon(getDownTrippleHexagonArray(this.centerX, this.centerY, this.edge, 0.9));
+            }
+
 
             this.$inner.attr({
                 fill: 'none',
@@ -73,5 +79,54 @@
             ];
         }
     }
+
+    function getUpTrippleHexagonArray(centerX, centerY, edge, ratio){
+        var dist = edge * Math.cos(Math.PI/6),
+            e = edge * ratio,
+            d = e * Math.cos(Math.PI/6);
+
+        var x1 = centerX,
+            y1 = centerY-edge,
+            x2 = centerX-dist,
+            y2 = centerY+edge/2,
+            x3 = centerX+dist,
+            y3 = centerY+edge/2;
+
+        return [
+
+        ];
+
+    }
+
+    function getDownTrippleHexagonArray(centerX, centerY, edge, ratio){
+        var dist = edge * Math.cos(Math.PI/6),
+            e = edge * ratio,
+            d = e * Math.cos(Math.PI/6);
+
+        var x1 = centerX-dist,
+            y1 = centerY-edge/2,
+            x2 = centerX+dist,
+            y2 = centerY-edge/2,
+            x3 = centerX,
+            y3 = centerY+edge;
+
+        return [
+            x1, y1-e,
+            x1+dist, centerY-e,
+            x2, y2-e,
+            x2+d, y2-e/2,
+            x2+d, y2+e/2,
+            x3+d, centerY+e/2,
+            x3+d, y3+e/2,
+            x3, y3+e,
+            x3-d, y3+e/2,
+            x3-d, centerY+e/2,
+            x1-d, y1+e/2,
+            x1-d, y1-e/2,
+            x1, y1-e
+        ];
+
+    }
+
 
 }());
