bbb0e73d79356b2c1c2abab67c3c7c2edb4003fe
ptx-pluto <sheep9275@gmail.com>
2014-08-23 00:36:22 +0800

    Mandelbrot canvas also adapted to browserify

--- src/scripts/base.js
+++ src/scripts/base.js
@@ -1,8 +1,9 @@
 var HexagonGrid = require('./hexagon-grid/HexagonGrid.js');
+var Mandelbrot = require('./graphs/Mandelbrot.js');
 
 window.Promise = Promise || RSVP.Promise;
 
 jQuery(function(){
-//    new PTX.Mandelbrot({});
-    new HexagonGrid({});
+    new Mandelbrot({});
+    //new HexagonGrid({});
 });
