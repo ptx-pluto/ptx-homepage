5eb15e6925867a6d543e71f4d954d365239406e6
ptx-pluto <sheep9275@gmail.com>
2014-10-12 15:12:42 +0800

    initTrippleTile added

--- src/scripts/app/views/ContentTileView.js
+++ src/scripts/app/views/ContentTileView.js
@@ -1,9 +1,120 @@
 'use strict';
 
-var HexagonTileView = require('./HexagonTileView.js');
+var TILE_TYPES = require('../content.js').TILE_TYPES,
+    SnapSvgMixin = require('./SnapSvgMixin.js'),
+    outlines = require('../../hexagon-grid/outlines.js');
 
-module.exports = Ember.View.extend(HexagonTileView, {
+module.exports = Ember.View.extend(SnapSvgMixin, {
+
+    tagName: 'svg',
+
+    container: Ember.computed.alias('parentView.parentView'),
+
+    grid: Ember.computed.alias('container.grid'),
+
+    didInsertElement: function(){
+        var handler = Snap(this.get('element'));
+        this.set('$snap', handler);
+    },
+
+    onLoaded: function(){
+
+        var grid = this.get('grid'),
+            tiles = this.get('tiles').map(function(tile){
+                return grid.getTile(tile.row, tile.col);
+            }),
+            _self = this;
+
+        switch (this.get('tileType')) {
+            case TILE_TYPES.SINGLE:
+                this.initSingle();
+                break;
+            case TILE_TYPES.TRIPPLE:
+                this.initTripple();
+                break;
+        }
+
+        Promise
+            .all(tiles.map(function(tile){
+                return tile.promiseReady();
+            }))
+            .then(function(){
+                return tiles.map(function(tile){
+                    return tile.promiseFade();
+                });
+            })
+            .then(function(){
+                return _self.promiseAppear();
+            });
+
+    }.observes('controller.isLoaded'),
+
+    initSingle: function(){
+        var url = this.get('image'),
+            handler = this.get('$snap'),
+            $img = handler.image(url, 0, 0, 350, 350),
+            $frame = handler.polygon(outlines.getHexagonArray(this.centerX, this.centerY, this.edge*0.8));
+
+        $img.attr({
+            'clip-path': this.$frame,
+            opacity: 0
+        });
+        $frame.attr({
+            fill: 'tomato'
+        });
+
+    },
+
+    initTripple: function(){
+
+        var tiles = this.get('tiles'),
+            grid = this.get('grid'),
+            config = outlines.getTrippleTileConfig(tiles, grid),
+            isUp = config.isUp,
+            position = config.position,
+            edge = this.get('grid.edge'),
+//            edges = Math.ceil(edge*12),
+            frameRatio = 0.8,
+            innerRatio = 0.9,
+            centerX = 2*edge * Math.cos(Math.PI/6),
+            centerY = isUp ? 2*edge : 1.5*edge,
+            url = this.get('image'),
+            handler = this.get('$snap');
+
+        handler.attr({
+            x: position[0],
+            y: position[1],
+            opacity: 0
+        });
+
+        var $inner = handler.polygon(outlines.getTripleHexagonArray(isUp, centerX, centerY, edge, innerRatio));
+        $inner.attr({
+            fill: 'none',
+            stroke: 'red',
+            'stroke-width': 5,
+            'stroke-linecap': 'square'
+        });
+
+        var $frame = handler.polygon(outlines.getTripleHexagonArray(isUp, centerX, centerY, edge, frameRatio));
+        $frame.attr({
+            fill: 'tomato'
+        });
+
+        var $img = handler.image(url, 0, 0, 350, 350);
+        $img.attr({
+            'clip-path': $frame
+        });
+
+    },
 
 
+//    promiseOutline: function(){
+//        return this.promiseAnimate(this.$inner, { 'stroke-dashoffset': 0 }, 1000);
+//    },
+
+
+    promiseAppear: function(){
+        return this.promiseAnimate(this.get('$snap'), { opacity: 1 }, 500);
+    }
 
 });
