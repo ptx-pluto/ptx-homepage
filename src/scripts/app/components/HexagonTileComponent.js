3efc075b1ac2270b56e87a1955f267e40a33897c
ptx-pluto <sheep9275@gmail.com>
2015-02-07 15:21:30 +0800

    g added

--- src/scripts/app/components/HexagonTileComponent.js
+++ src/scripts/app/components/HexagonTileComponent.js
@@ -8,9 +8,9 @@ var outlines = require('../../hexagon-grid/outlines.js'),
 
 module.exports = Ember.Component.extend({
 
-    tagName: 'polygon',
+    tagName: 'g',
 
-    classNames: ['component__hexagon-tile'],
+//    classNames: ['component__hexagon-tile'],
 
     attributeBindings: ['points'],
 
@@ -38,7 +38,42 @@ module.exports = Ember.Component.extend({
 
     points: function() {
 
-        var RATIO = 0.9;
+        var RATIO = 0.95;
+
+        var row = this.get('row'),
+            col = this.get('col'),
+            grid = this.get('grid');
+
+        //if (!row || !col || !grid) throw "Require row, col and grid to initialize the tile";
+
+        var edge = this.get('edge'),
+            center = this.get('center'),
+            centerX = center[0],
+            centerY = center[1];
+
+        var outline = outlines.getHexagonArray(centerX, centerY, edge*RATIO);
+
+        return outline.reduce(function(memo, val, index){
+            if (index === 0) {
+
+            }
+            else if (index % 2 === 0) {
+                memo += ' ';
+            }
+            else {
+                memo += ',';
+            }
+
+            return memo + val;
+
+        }, '');
+
+    }.property('row', 'col'),
+
+
+    innerOutline: function() {
+
+        var RATIO = 0.85;
 
         var row = this.get('row'),
             col = this.get('col'),
@@ -72,19 +107,21 @@ module.exports = Ember.Component.extend({
 
     appear: function(){
 
-        var handler = Snap(this.get('element')),
+        var g = jQuery(this.get('element')),
+            $outter = Snap(jQuery('.component__hexagon-tile', g)[0]),
+            $inner = Snap(jQuery('.component__hexagon-tile__inner', g)[0]),
             row = this.get('row'),
             col = this.get('col'),
             edges = this.get('edge')*6;
 
-        handler.attr({
+        $outter.attr({
             'stroke-dashoffset': edges,
             'stroke-dasharray': edges
         });
 
         _.delay(function(){
-            handler.animate({ 'stroke-dashoffset': 0 }, TILE_OUTLINE_DURATION, null, function(){
-                handler.animate({ fill: '#FF0526' }, 500, null, function(){});
+            $outter.animate({ 'stroke-dashoffset': 0 }, TILE_OUTLINE_DURATION, null, function(){
+                $inner.animate({ fill: '#FF0526' }, 500, null, function(){});
             });
         }, TILE_OUTLINE_DURATION/3*(row+col)+1);
 
