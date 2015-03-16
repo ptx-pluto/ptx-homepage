697263c1136026ab2c6f12274fbff2e39de323f3
ptx-pluto <sheep9275@gmail.com>
2015-03-16 20:26:45 +0800

    background image for tile added

--- src/scripts/app/components/HexagonTileComponent.js
+++ src/scripts/app/components/HexagonTileComponent.js
@@ -6,6 +6,8 @@ var outlines = require('../../hexagon-grid/outlines.js'),
     settings = require('../settings'),
     TILE_OUTLINE_DURATION = settings.TILE_OUTLINE_DURATION;
 
+var INNER_RATIO = 0.85;
+
 module.exports = Ember.Component.extend({
 
     tagName: 'g',
@@ -22,21 +24,56 @@ module.exports = Ember.Component.extend({
 
     edge: Ember.computed.alias('grid.edge'),
 
+    uuid: function(){
+        return _.uniqueId('hexagon-tile-');
+    }.property('row', 'col'),
 
-    center: function(){
+    clip1: function(){
+        return 'url(#' + this.get('clip1id') + ')';
+    }.property('clip1id'),
 
+    clip1id: function(){
+        return this.get('uuid') + '__clip1';
+    }.property('uuid'),
+
+    corner: function(){
         var row = this.get('row'),
             col = this.get('col'),
             edge = this.get('edge'),
-            pos = this.get('grid').getTilePosition(row, col),
-            x = pos[0], y = pos[1],
-            deltaX = edge * Math.cos(Math.PI/6),
-            deltaY = edge;
+            pos = this.get('grid').getTilePosition(row, col);
+        return {
+            x: pos[0],
+            y: pos[1]
+        };
+    }.property('row', 'col'),
 
-        return [ x+deltaX, y+deltaY ];
+    /*
+    innerWidth: function(){
+        return this.get('edge')*Math.sqrt(3)*INNER_RATIO;
+    }.property('edge'),
 
-    }.property('row', 'col', 'edge'),
+    innerHeight: function(){
+        return this.get('edge')*2*INNER_RATIO;
+    }.property('edge'),
 
+    innerCorner: function(){
+        var corner = this.get('corner'),
+            edge = this.get('edge');
+        return {
+            x: corner.x + Math.sqrt(3)/2*edge*(1-INNER_RATIO),
+            y: corner.y + edge*(1-INNER_RATIO)
+        };
+    }.property('corner'),
+    */
+
+    center: function(){
+        var edge = this.get('edge'),
+            pos = this.get('corner');
+        return [
+            pos.x + edge * Math.cos(Math.PI/6),
+            pos.y + edge
+        ];
+    }.property('corner', 'edge'),
 
     points: function() {
 
@@ -80,9 +117,13 @@ module.exports = Ember.Component.extend({
             'stroke-dasharray': edges
         });
 
+        $inner.attr({
+            'opacity': 0
+        });
+
         _.delay(function(){
             $outter.animate({ 'stroke-dashoffset': 0 }, TILE_OUTLINE_DURATION, null, function(){
-                $inner.animate({ fill: '#FF0526' }, 500, null, function(){
+                $inner.animate({ opacity: 1 }, 500, null, function(){
                     grid.send('tileReady', row, col, _self);
                 });
             });
