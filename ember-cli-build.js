1dabecfad44098175525cd40d624501c45087ed4
pu.xu <pu.xu@dji.com>
2015-08-23 22:02:13 +0800

    snap.svg added

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,11 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('bower_components/snap.svg/dist/snap.svg-min.js', {
+    type: 'vendor',
+    prepend: true
+  });
+
   var assets = new Funnel('assets', {
     srcDir: '/',
     destDir: 'assets'
